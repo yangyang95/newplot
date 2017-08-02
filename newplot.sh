ae7c7270e6fc65faa426b7954592dc172367a5bb
yangyang95 <yangyang.weng@gmail.com>
2017-08-02 13:47:37 +0800

    Create project

---
+++ newplot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Name:		newplot
+# version:	0.1
+# Purpose:	Create sample plot for gnuplot
+# Author: 	Jerry Weng yangyang.weng@gmail.com
+# License:	GNU General Public License v3.0
+# 			https://www.gnu.org/licenses/gpl-3.0.en.html
+#------------------------------------------------------------------------------
